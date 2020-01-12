7fbc54a2b4fc393d3b9f292e21caada40ed37f60
uralrdybread <48900828+uralrdybread@users.noreply.github.com>
2020-01-11 23:58:22 -0800

    Add files via upload

---
+++ bakingconverter.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[7]:
+
+
+print("How many cups of bread flour would you like to convert to grams?")
+cups = input()
+grams = float(cups)*130
+grams = round(grams, 2)
+print(f"{cups} cups is equal to {grams} grams ")
+
